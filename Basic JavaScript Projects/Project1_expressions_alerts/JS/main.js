0de3ab51f99dba82cc4a0a1eaa61cba6b1258ce3
heyitspham <viquph@gmail.com>
2021-07-19 01:16:11 -0400

    Practicing basic JS for start of course.

---
+++ Basic JavaScript Projects/Project1_expressions_alerts/JS/main.js
@@ -0,0 +1,27 @@
+document.write("Hello, World!");
+document.write("<br>") //inserted a line-break
+
+var A = "This is a string"; //defined a variable and assigned it string values
+document.write(A); //practicing the document.write method
+document.write("<br>")
+
+var B = "This is a string for the window.alert method";
+window.alert(B); //practicing the window.alert method
+
+var C = "Concatenated" + "String"; //concatenated two strings together and assigned them to variable C
+document.write(C);
+document.write("<br>")
+
+var Family = "Phams", Dad = "Lam", Mom = "Kim", Sister= "Trang", Brother="Tung"; //defined multiple variables (along with assigning them string values)
+Sister = Sister.fontcolor("purple"); // changed the value (of the variable Sister) to the color purple, using the fontcolor method
+document.write(Sister);
+document.write("<br>")
+
+var feelingBlue = "I have the blues.";
+var feelingBlue = feelingBlue.fontcolor("blue");
+document.write(feelingBlue);
+document.write("<br>")
+
+var addingResults = 3 + 4; //an expression of "3 + 4", where its result is then assigned to the variable addingResults.
+document.write(addingResults);
+document.write("<br>")
