549fad989c513f50989f47226bba3e988e908782
heyitspham <viquph@gmail.com>
2021-07-21 00:15:34 -0400

    Added comments.

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,36 +1,36 @@
 function Ride_Function() {
     var Height, CanRide;
-    Height = document.getElementById("InputAge").value;
-    CanRide = (Height < 18) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = CanRide + " to ride.";
+    Height = document.getElementById("Height").value; //sets number variable Height to the value set in the targeted input element.
+    CanRide = (Height < 18) ? "You are too short":"You are tall enough"; //sets string variable CanRide to either string value options depending on the value of Height entered.
+    document.getElementById("Ride").innerHTML = CanRide + " to ride."; //prints the string values in CanRide concatenated with " to ride."
 }
 
 function AgeFunction() {
     var Age, CanVote;
-    Age = document.getElementById("InputAge").value;
-    CanVote = (Age < 18) ? "You cannot":"You can";
-    document.getElementById("Age").innerHTML = CanVote + " vote.";
+    Age = document.getElementById("InputAge").value; //sets number variable Age to the value set in the targeted input element.
+    CanVote = (Age < 18) ? "You cannot":"You can"; //sets string variable CanVote to either string value options depending on the value of Age entered.
+    document.getElementById("Age").innerHTML = CanVote + " vote."; //prints the string values in CanVote concatenated with " vote."
 }
 
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
+    this.Vehicle_Color = Color; //added four objects to THIS function Vehicle.
 }
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard"); //created 3 new instances of the function Vehicle() and assigned values to their objects.
 function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year + ".";
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year + "."; //prints strings.
 }
 
 function count_Function() {
-    document.getElementById("Nested_Function").innerHTML = Count();
+    document.getElementById("Nested_Function").innerHTML = Count(); //prints the value from the function Count().
     function Count() {
         var Starting_point = 9;
-        function Plus_one() {Starting_point += 1;}
-        Plus_one();
-        return Starting_point;
+        function Plus_one() {Starting_point += 1;} //function Plus_one() to increment number variable Starting_point by 1.
+        Plus_one(); //calls Plus_one(), thus incrementing number variable Starting_point by 1, which sets Starting_point to 10.
+        return Starting_point; //10 is returned for the function Count().
     }
 }
